c7af2d60c1bbdddde5301a299d7663aaca05f150
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-07 18:30:12 +0900

    kang 9회차 문제풀이

--- BOJ/src/기본수학2/소수구하기_1929/kang_1929.java
+++ BOJ/src/기본수학2/소수구하기_1929/kang_1929.java
@@ -1,31 +1,59 @@
 package 기본수학2.소수구하기_1929;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import static java.lang.Math.sqrt;
+
+// 496ms
+// 1부터 1000001 까지 소수인지 아닌지 판별하는 배열을 미리 만듦
 public class kang_1929 {
 
     static boolean[] primeNum = new boolean[1000001];
+    // true -> 소수
+    // false -> 소수 X
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    // 배열의 인덱스가 소수면 true, 아니라면 false
     public static void primeFunc() {
+        for(int i = 2; i < primeNum.length; i++) {
+            boolean find = false; // 소수가 아님 -> true
+            if(i>3) {
+                for(int j =2; j <= sqrt(i); j++) {
 
-        for(int i = 1; i < primeNum.length; i++) {
-            if(i == 2) {
-                primeNum[2] = true;
-            } else if(i==3) {
-                
+                    // 소수가 아님
+                    if(i % j == 0) {
+                        find = true;
+                        break;
+                    }
+                }
+            }
+            if(!find) {
+                primeNum[i] = true;
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        st = new StringTokenizer(br.readLine());
 
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
+        primeFunc();
+
+        for(int i = M; i <= N; i++) {
+            if(primeNum[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.println(sb);
     }
 }
---
+++ out/production/BOJ/재귀/팩토리얼_10872/README.md
@@ -0,0 +1,19 @@
+# Platform
+
+* BAEKJOON
+
+# Category
+
+* Recursive Function(재귀)
+
+# Problem_name
+
+* Factorial(팩토리얼)
+
+# Problem_num
+
+* 10872
+
+# Level
+
+* Bronze V
