dceae3fdbe74b86d2b84105e5af7a343689fcac7
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-10-17 10:46:01 +0900

    KJames programmer Level1 풀이

---
+++ programmers/src/level1/스킬테스트2.java
@@ -0,0 +1,12 @@
+package level1;
+
+// a[0]*b[0] + a[1]*b[1] + ... + a[n]*b[n]
+public class 스킬테스트2 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
