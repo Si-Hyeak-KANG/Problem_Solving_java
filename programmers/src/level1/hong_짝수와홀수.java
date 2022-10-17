0372ef0f61c22f4b9023a4f3a8baa7d00ef15a5e
hongmj37 <alswjd3813@naver.com>
2022-10-17 14:01:19 +0900

    hong_programmers_짝수와 홀수_문제설명 추가

--- programmers/src/level1/hong_짝수와홀수.java
+++ programmers/src/level1/hong_짝수와홀수.java
@@ -1,5 +1,9 @@
 package level1;
 
+/**
+ * 짝수와 홀수
+ * 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+ */
 public class hong_짝수와홀수 {
         public String solution(int num) {
             String answer = "";
