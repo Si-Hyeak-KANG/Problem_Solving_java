a5f9456f98e620c39d95209b93a756c131789c41
hongmj37 <alswjd3813@naver.com>
2022-10-26 09:25:50 +0900

    no message

--- programmers/src/level1/hong_약수의합.java
+++ programmers/src/level1/hong_약수의합.java
@@ -1,5 +1,9 @@
 package level1;
 
+/** 프로그래머스 코딩테스트연습 level1
+ * 약수의합
+ * 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+ */
 public class hong_약수의합 {
     public int solution(int n) {
         int answer = 0;
