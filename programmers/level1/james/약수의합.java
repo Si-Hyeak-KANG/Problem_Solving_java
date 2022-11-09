be1390f65c37ba487c58ddfc56a6221e68122e0e
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-11-09 17:40:09 +0900

    kang_약수의합 풀이

--- programmers/level1/james/약수의합.java
+++ programmers/level1/james/약수의합.java
@@ -1,5 +1,10 @@
 package level1.james;
 
+/**
+ * 약수 합을 구하는 문제에서 발생하는 예외
+ * 1) n이 1일 경우
+ * 2) 제곱수
+ */
 public class 약수의합 {
     public int solution(int n) {
         int answer = 0;
@@ -7,8 +12,10 @@ public class 약수의합 {
         for (int i = 1; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 answer += i;
-                int anotherValue = n / i;
-                answer += anotherValue;
+                if(i != Math.sqrt(n)) {
+                    int anotherValue = n / i;
+                    answer += anotherValue;
+                }
             }
         }
         return answer;
