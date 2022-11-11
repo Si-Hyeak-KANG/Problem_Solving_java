0f5e5b7dddea61447e66db69edf8a38961114482
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-11-11 10:00:08 +0900

    kang 다리를 지나는 트럭 풀이

--- programmers/level2/james/다리를_지나는_트럭.java
+++ programmers/level2/james/다리를_지나는_트럭.java
@@ -22,34 +22,39 @@ public class 다리를_지나는_트럭 {
     }
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-
-        int count = 0;
+        int time = 0;
         Queue<Integer> bridge = new LinkedList<>();
         int index = 0;
 
-        while (index < getLength(truck_weights)) {
+        while(index < truck_weights.length) {
             int currTruck = truck_weights[index];
-            int currWeightInBridge = bridge.stream().mapToInt(m -> m).sum();
-            int expectWeight = currWeightInBridge + currTruck;
 
-            if (bridge.size() < bridge_length) {
-                if (expectWeight <= weight) {
+            if(bridge.isEmpty()) {
+                bridge.add(currTruck);
+                index++;
+                time++;
+            } else if(bridge.size() < bridge_length) {
+
+                if(isPermitWeight(getCurrWeightInBridge(bridge), currTruck, weight)) {
                     bridge.add(currTruck);
-                    count++;
                     index++;
+                    time++;
                 } else {
-                    bridge.poll();
-                    count += bridge_length - 1;
+                    bridge.add(0);
+                    time++;
                 }
             } else {
                 bridge.poll();
             }
         }
-        count += bridge_length;
-        return count;
+        return time + bridge_length;
     }
 
-    private int getLength(int[] truck_weights) {
-        return truck_weights.length;
+    public int getCurrWeightInBridge(Queue<Integer> bridge) {
+        return bridge.stream().mapToInt(a->a).sum();
+    }
+
+    public boolean isPermitWeight(int currWeightInBridge, int currTruckWeight, int limit) {
+        return currWeightInBridge + currTruckWeight <= limit;
     }
 }
